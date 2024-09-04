a1a0ebfbdcef6d0dc848ba15a75aac7ea75de964
Melida Zuniga <melidazuniga55@outlook.com>
2024-09-04 12:05:33 -0600

    Correccion en index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 function calcular() {
-    var numero1 = document.getElementById("txtNumero1")
-    var numero2 = document.getElementById("txtNumero2")
+    var numero1 = document.getElementById("txtNumero1");
+    var numero2 = document.getElementById("txtNumero2");
 
     var total = parseFloat (numero1.value) + parseFloat (numero2.value);
 
